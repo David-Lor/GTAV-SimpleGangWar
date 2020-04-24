68e0872c9993ebc8e488d37dc7396c12241b5430
David-Lor <17401854+david-lor@users.noreply.github.com>
2020-04-24 21:16:55 +0200

    Add names to blips; Set health & armor to peds of each team; Option to disable wanted level during gang war; Option to select ped task behaviour; Option to select combat movement for each team; Different script Tick intervals for idle or in-battle

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 Grand Theft Auto V script to create a basic battle between two teams.
 
+## Background
+
+This script is inspired by [Kerosini's Gang War script](https://www.gta5-mods.com/scripts/gangwar-zip).
+The focus of my script is to provide a similar simple script to create instant battles, while providing new customizable settings.
+
 ## Installing
 
 - Download and extract the required dependencies: ScriptHookV & ScriptHookVDotNet
@@ -9,23 +14,38 @@ Grand Theft Auto V script to create a basic battle between two teams.
 
 ## Usage
 
-The key `B` can be used to navigate through all the steps of the script. In-game help popups will describe what to do, but these are the different stages you will find:
+The key `B` is used to navigate through all the steps of the script. In-game help popups will describe what to do, but these are the different stages you will find:
 
 1. The script will ask you to move to where the enemies will spawn
 2. After pressing the hotkey, you must do the same to define where the allies will spawn
 3. Right after defining both spawnpoints, peds from both teams will spawn on their respective spawnpoints, and fight each other
 4. Press the hotkey once to enter the "exit mode" (it will ask for confirmation to stop the battle)
-5. The last hotkey press will inmediately stop the battle and remove all alive & dead peds from the map
+5. Pressing the hotkey again will inmediately stop the battle and remove all alive & dead peds from the map
 
 ## TODO
 
-- Setup teams and other settings through a .ini file
-- Allow to pause & resume ped spawn
+- Setup teams and other settings through a .ini file; Document settings
+- Allow to pause & resume ped spawning
 - Avoid spawn-killing
+- Add winning conditions
+- Smooth transition from battle end to cleanup (extra step?)
 - Fix peds being idle if spawnpoints are too far (change or combine task to RunTo?)
 - Add menu/more hotkeys to improve UX?
+- Respawn on ally spawnpoint after player dies
+- Organize data, settings, variables - for each teams on the script structurally (struct?)
 
 ## Changelog
 
+- 0.1.1
+	- Add names to blips
+	- Set health & armor to peds of each team
+	- Option to disable wanted level during gang war
+	- Option to select ped task behaviour
+	- Option to select combat movement for each team
+	- Different script Tick intervals for idle or in-battle
 - 0.0.1 - Initial release
 
+## Known bugs
+
+- If spawnpoints are too far away from each other, peds can idle and do nothing
+- When using [Watch Your Death](https://gta5-mods.com/scripts/watch-your-death), while player is dead, enemies can run to ally spawnpoint without fighting, or be idle
--- SimpleGangWar.cs
+++ SimpleGangWar.cs
@@ -9,28 +9,60 @@ public class SimpleGangWar : Script {
 
     // Peds: https://github.com/crosire/scripthookvdotnet/blob/d1827497495567d810986aa752f8d903853088fd/source/scripting_v2/GTA.Native/PedHash.cs
     // Weapons: https://github.com/crosire/scripthookvdotnet/blob/d1827497495567d810986aa752f8d903853088fd/source/scripting_v2/GTA.Native/WeaponHash.cs
+    // Peds with images (do not use the exact names listed here): https://wiki.gtanet.work/index.php/Peds - https://docs.fivem.net/docs/game-references/ped-models
+
+    // Families (allies) vs Ballas (enemies)
     private static readonly string[] pedsAllies = { "Families01GFY", "Famca01GMY", "Famdnf01GMY", "Famfor01GMY" };
-    private static readonly string[] weaponsAllies = { "AssaultRifle", "CompactRifle", "DoubleBarrelShotgun", "SNSPistol" };
+    private static readonly string[] weaponsAllies = { "AssaultRifle", "CompactRifle", "SNSPistol", "VintagePistol", "Pistol", "MicroSMG" };
     private static readonly string[] pedsEnemies = { "BallaEast01GMY", "BallaOrig01GMY", "Ballas01GFY", "BallaSout01GMY" };
-    private static readonly string[] weaponsEnemies = { "MicroSMG", "MachinePistol", "Gusenberg", "Musket", "SawnOffShotgun" };
-    private static readonly int healthAllies = 75;
+    private static readonly string[] weaponsEnemies = { "MicroSMG", "MachinePistol", "Gusenberg", "Musket", "Pistol", "VintagePistol", "CompactRifle" };
+    
+    // Misc gangs (allies) vs Misc security forces (enemies)
+    /*private static readonly string[] pedsAllies = { "Families01GFY", "Famca01GMY", "Famdnf01GMY", "Famfor01GMY", "BallaEast01GMY", "BallaOrig01GMY", "Ballas01GFY", "BallaSout01GMY", "Vagos01GFY", "Lost01GFY", "Lost01GMY", "Lost02GMY", "Lost03GMY" };
+    private static readonly string[] weaponsAllies = { "AssaultRifle", "CompactRifle", "Pistol", "SNSPistol", "VintagePistol", "MicroSMG", "MG", "MachinePistol" };
+    private static readonly string[] pedsEnemies = { "BogdanGoon", "AvonGoon", "Blackops01SMY", "Blackops02SMY", "Blackops03SMY", "Marine03SMY", "Devinsec01SMY", "SecuroGuardMale01", "Armoured01SMM", "Armoured02SMM", "Armoured01", "ChemSec01SMM" };
+    private static readonly string[] weaponsEnemies = { "CarbineRifle", "CarbineRifleMk2", "CombatMG", "CombatMGMk2", "PumpShotgunMk2", "PistolMk2", "RevolverMk2", "SMG", "SMGMk2", "CombatPDW" };*/
+
+	// Ballas (allies) vs Families (enemies)
+	/*private static readonly string[] pedsAllies = { "BallaEast01GMY", "BallaOrig01GMY", "Ballas01GFY", "BallaSout01GMY" };
+    private static readonly string[] weaponsAllies = { "AssaultRifle", "CompactRifle", "Gusenberg", "SNSPistol", "Pistol", "VintagePistol", "MachinePistol", "MicroSMG", "SMG" };
+    private static readonly string[] pedsEnemies = { "Families01GFY", "Famca01GMY", "Famdnf01GMY", "Famfor01GMY" };
+    private static readonly string[] weaponsEnemies = { "AssaultRifle", "CompactRifle", "Gusenberg", "SNSPistol", "Pistol", "VintagePistol", "MachinePistol", "MicroSMG", "SMG" };*/
+
+    private static readonly int healthAllies = 120;
     private static readonly int armorAllies = 0;
-    private static readonly int healthEnemies = 75;
+    private static readonly int healthEnemies = 120;
     private static readonly int armorEnemies = 0;
     private static readonly int accuracyAllies = 5;
     private static readonly int accuracyEnemies = 5;
+    private static readonly CombatMovement combatMovementAllies = CombatMovement.Offensive;
+    private static readonly CombatMovement combatMovementEnemies = CombatMovement.Offensive;
 
-    private static readonly uint maxPedsPerTeam = 5;
+    private static readonly int maxPedsPerTeam = 10;
     private static readonly Keys hotkey = Keys.B;
-    private static readonly bool blips = true;
+    private static readonly bool noWantedLevel = true;
+    private static readonly bool showBlipsOnPeds = true;
     private static readonly bool dropWeaponOnDead = false;
     private static readonly bool removeDeadPeds = true;
+    private static readonly bool runToSpawnpoint = true;
+    private static readonly int idleInterval = 500;
+    private static readonly int battleInterval = 100;
 
     private int relationshipGroupAllies;
     private int relationshipGroupEnemies;
+    private int originalWantedLevel;
     private List<Ped> spawnedAllies = new List<Ped>();
     private List<Ped> spawnedEnemies = new List<Ped>();
     private List<Ped> deadPeds = new List<Ped>();
+    private List<Ped> pedsRemove = new List<Ped>();
+
+    private enum CombatMovement {
+        // https://runtime.fivem.net/doc/natives/?_0x4D9CA1009AFBD057
+        Stationary = 0,
+        Defensive = 1,
+        Offensive = 2,
+        Suicidal = 3
+    }
 
     private enum Stage {
         Initial = 0,
@@ -52,15 +84,16 @@ public class SimpleGangWar : Script {
     public SimpleGangWar() {
         Tick += OnTick;
         KeyUp += OnKeyUp;
-        Interval = 500;
+        Interval = idleInterval;
 
         relationshipGroupAllies = World.AddRelationshipGroup("simplegangwar_allies");
         relationshipGroupEnemies = World.AddRelationshipGroup("simplegangwar_enemies");
-        World.SetRelationshipBetweenGroups(Relationship.Respect, relationshipGroupAllies, relationshipGroupAllies);
-        World.SetRelationshipBetweenGroups(Relationship.Respect, relationshipGroupEnemies, relationshipGroupEnemies);
-        World.SetRelationshipBetweenGroups(Relationship.Hate, relationshipGroupAllies, relationshipGroupEnemies);
-        World.SetRelationshipBetweenGroups(Relationship.Respect, relationshipGroupAllies, Game.Player.Character.RelationshipGroup);
-        World.SetRelationshipBetweenGroups(Relationship.Hate, relationshipGroupEnemies, Game.Player.Character.RelationshipGroup);
+        int relationshipGroupPlayer = Game.Player.Character.RelationshipGroup;
+        SetRelationshipBetweenGroups(Relationship.Hate, relationshipGroupAllies, relationshipGroupEnemies);
+        SetRelationshipBetweenGroups(Relationship.Respect, relationshipGroupAllies, relationshipGroupAllies);
+        SetRelationshipBetweenGroups(Relationship.Respect, relationshipGroupEnemies, relationshipGroupEnemies);
+        SetRelationshipBetweenGroups(Relationship.Respect, relationshipGroupAllies, relationshipGroupPlayer);
+        SetRelationshipBetweenGroups(Relationship.Hate, relationshipGroupEnemies, relationshipGroupPlayer);
 
         UI.Notify("SimpleGangWar loaded");
     }
@@ -69,8 +102,9 @@ public class SimpleGangWar : Script {
         if (stage >= Stage.Running) {
             SpawnPeds(true);
             SpawnPeds(false);
-            ProcessSpawnedPeds(spawnedAllies);
-            ProcessSpawnedPeds(spawnedEnemies);
+            
+            ProcessSpawnedPeds(true);
+            ProcessSpawnedPeds(false);
         }
     }
 
@@ -85,7 +119,7 @@ public class SimpleGangWar : Script {
                 stage = Stage.EnemySpawnpointDefined;
             } else if (stage == Stage.EnemySpawnpointDefined) {
                 DefineSpawnpoint(true);
-                spawnpointsDistance = spawnpointEnemies.DistanceTo(spawnpointAllies) * 1.5f;
+                SetupBattle();
                 UI.ShowHelpMessage("The battle begins NOW!", 5000, true);
                 stage = Stage.Running;
             } else if (stage == Stage.Running) {
@@ -99,6 +133,16 @@ public class SimpleGangWar : Script {
         }
     }
 
+    private void SetupBattle() {
+        Interval = battleInterval;
+        spawnpointsDistance = spawnpointEnemies.DistanceTo(spawnpointAllies);
+
+        if (noWantedLevel) {
+            originalWantedLevel = Game.MaxWantedLevel;
+            Game.MaxWantedLevel = 0;
+        }
+    }
+
     private void SpawnPeds(bool alliedTeam) {
         List<Ped> spawnedPedsList = alliedTeam ? spawnedAllies : spawnedEnemies;
 
@@ -109,8 +153,8 @@ public class SimpleGangWar : Script {
 
     private Ped SpawnRandomPed(bool alliedTeam) {
         Vector3 pedPosition = alliedTeam ? spawnpointAllies : spawnpointEnemies;
-        string pedName = randomChoice<string>(alliedTeam ? pedsAllies : pedsEnemies);
-        string weaponName = randomChoice<string>(alliedTeam ? weaponsAllies : weaponsEnemies);
+        string pedName = RandomChoice<string>(alliedTeam ? pedsAllies : pedsEnemies);
+        string weaponName = RandomChoice<string>(alliedTeam ? weaponsAllies : weaponsEnemies);
         PedHash pedSpawn;
         WeaponHash weaponGive;
 
@@ -125,31 +169,34 @@ public class SimpleGangWar : Script {
         Ped ped = World.CreatePed(pedSpawn, pedPosition);
         ped.Weapons.Give(weaponGive, 1, true, true);
 
-        // TODO Set health, armor (they die on spawn if changed)
-        //createdPed.Health = alliedTeam ? healthAllies : healthEnemies;
-        //createdPed.Armor = createdPed.Armor = alliedTeam ? armorAllies : armorEnemies;
+        ped.Health = ped.MaxHealth = alliedTeam ? healthAllies : healthEnemies;
+        ped.Armor = alliedTeam ? armorAllies : armorEnemies;
         ped.Money = 0;
         ped.Accuracy = alliedTeam ? accuracyAllies : accuracyEnemies;
         ped.RelationshipGroup = alliedTeam ? relationshipGroupAllies : relationshipGroupEnemies;
         ped.DropsWeaponsOnDeath = dropWeaponOnDead;
+
         Function.Call(Hash.SET_PED_COMBAT_ATTRIBUTES, ped, 46, true);  // force peds to fight
         Function.Call(Hash.SET_PED_SEEING_RANGE, ped, spawnpointsDistance);
-        //Function.Call(Hash.SET_PED_COMBAT_RANGE, ped, 2);  // combat range: Far
+        //Function.Call(Hash.SET_PED_COMBAT_RANGE, ped, 0);  // 0=near, 2=far
+        Function.Call(Hash.SET_PED_COMBAT_MOVEMENT, ped, (int)(alliedTeam ? combatMovementAllies : combatMovementEnemies));
 
-        if (blips) {
+        if (showBlipsOnPeds) {
             Blip blip = ped.AddBlip();
             blip.Color = alliedTeam ? BlipColor.Blue : BlipColor.Orange;
+            blip.Name = alliedTeam ? "Ally team member" : "Enemy team member";
             blip.Scale = 0.5f;
         }
 
         ped.Task.ClearAllImmediately();
+        ped.AlwaysKeepTask = true;
         (alliedTeam ? spawnedAllies : spawnedEnemies).Add(ped);
 
         return ped;
     }
 
-    private void ProcessSpawnedPeds(List<Ped> pedList) {
-        List<Ped> pedsRemove = new List<Ped>();
+    private void ProcessSpawnedPeds(bool alliedTeam) {
+        List<Ped> pedList = alliedTeam ? spawnedAllies : spawnedEnemies;
 
         foreach (Ped ped in pedList) {
             if (ped.IsDead) {
@@ -158,14 +205,16 @@ public class SimpleGangWar : Script {
                 deadPeds.Add(ped);
                 if (removeDeadPeds) ped.MarkAsNoLongerNeeded();
             } else if (ped.IsIdle) {
-                // TODO peds can stuck at idle if spawnpointsDistance is too far; should have an optional task of RunTo
-                ped.Task.FightAgainstHatedTargets(spawnpointsDistance);
+                if (runToSpawnpoint) ped.Task.RunTo(alliedTeam ? spawnpointEnemies : spawnpointAllies);
+                else ped.Task.FightAgainstHatedTargets(spawnpointsDistance);
             }
         }
 
         foreach (Ped ped in pedsRemove) {
             pedList.Remove(ped);
         }
+
+        pedsRemove.Clear();
     }
 
     private void DefineSpawnpoint(bool alliedTeam) {
@@ -177,11 +226,13 @@ public class SimpleGangWar : Script {
             spawnpointBlipAllies = blip;
             blip.Sprite = BlipSprite.TargetA;
             blip.Color = BlipColor.Blue;
+            blip.Name = "Ally spawnpoint";
         } else {
             spawnpointEnemies = position;
             spawnpointBlipEnemies = blip;
             blip.Sprite = BlipSprite.TargetE;
             blip.Color = BlipColor.Orange;
+            blip.Name = "Enemy spawnpoint";
         }
     }
 
@@ -192,6 +243,7 @@ public class SimpleGangWar : Script {
     }
 
     private void Teardown() {
+        Interval = idleInterval;
         spawnpointBlipAllies.Remove();
         spawnpointBlipEnemies.Remove();
 
@@ -202,9 +254,16 @@ public class SimpleGangWar : Script {
         spawnedAllies.Clear();
         spawnedEnemies.Clear();
         deadPeds.Clear();
+
+        if (noWantedLevel) Game.MaxWantedLevel = originalWantedLevel;
     }
 
-    private T randomChoice<T>(T[] array) {
+    private void SetRelationshipBetweenGroups(Relationship relationship, int groupA, int groupB) {
+        World.SetRelationshipBetweenGroups(relationship, groupA, groupB);
+        World.SetRelationshipBetweenGroups(relationship, groupB, groupA);
+    }
+
+    private T RandomChoice<T>(T[] array) {
         return array[random.Next(0, array.Length)];
     }
 }
